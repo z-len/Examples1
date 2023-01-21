f2837998fc579e25d692f7b634ede47b626505c3
Elena Zosina <z-len@yandex.ru>
2023-01-21 18:29:29 +0300

    add Condition

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите имя:");
+string username = Console.ReadLine();
+if (username. ToLower()== "маша") // ToLower() переводит все символы в нижний регистр
+{
+
+    Console.WriteLine("Ура! Это же Маша!");
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
+
+
+
