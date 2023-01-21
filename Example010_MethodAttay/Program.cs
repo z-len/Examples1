59804a0ddef615f9087ff35f3786e2734678ecfb
Elena Zosina <z-len@yandex.ru>
2023-01-21 23:19:16 +0300

    add example010 поиск индекса элемента в массиве

---
+++ Example010_MethodAttay/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 10, 21, 34, 46, 5, 689, 70, 8, 70 };
+int n = array.Length;
+int find = 70;
+int index = 0;
+while(index<n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
