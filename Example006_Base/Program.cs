f37dc8f529bfbb9d20ef9afc16e9bdc7a01fce61
Elena Zosina <z-len@yandex.ru>
2023-01-21 18:45:23 +0300

    add Example006_Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 1;
+int b = 4;
+int c = 200;
+int d = 34;
+int e = 97;
+int f = 9;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+if (f > max) max = f;
+
+Console.Write("max = ");
+Console.WriteLine(max);
