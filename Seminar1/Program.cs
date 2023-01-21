2d11846b4be3243cdeab5de0e6f2ee63fa0e384c
Elena Zosina <z-len@yandex.ru>
2023-01-22 00:20:45 +0300

    seminar1 нахождение квадрата числа математически и через бибилиотеку

---
+++ Seminar1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу ввода числа, которая на выходе выдает его квадрат
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()); // конвертация строки в целочисленный тип
+
+// 1. Математическое решение
+
+int num = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {num}.");
+
+// Решение через библиотеку
+int num2 = Convert.ToInt32(Math.Pow(number, 2));
+
+Console.WriteLine($"Квадрат числа {number} равен {num2}.");
+
+
+
