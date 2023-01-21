5f36ecff4db1dd0ceaf7bad85a058f27b36b2c18
Elena Zosina <z-len@yandex.ru>
2023-01-21 23:44:27 +0300

    Example011 заполнение и печать массива

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,28 @@
+﻿void FillArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while(index<Length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position<count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10]; // это значит "создай новый массив, в котором будет 10 элементов
+// по умолчанию он будет наполнен нулями
+
+FillArray(array);
+PrintArray(array);
+
