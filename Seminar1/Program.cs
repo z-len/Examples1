c798d48ba6f96f9c2ea5fe9ef279e445aad4ab87
Elena Zosina <z-len@yandex.ru>
2023-01-22 17:14:11 +0300

    22.01.2023

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -13,21 +13,21 @@
 // Console.WriteLine($"Квадрат числа {number} равен {num2}.");
 
 
-// // На вход принимаем число n, на выходе дает все целые числа в промежутке от -n до n
+// На вход принимаем число n, на выходе дает все целые числа в промежутке от -n до n
 
-// Console.WriteLine("Введите число: ");
-// int n = int.Parse(Console.ReadLine());
-// int count = -n;
-// while(count<=n)
-// {
-//    Console.WriteLine(count);
-//     count++;
-// }
-
-//Написать программу, которая на вход принимает трехзначное число и на выходе дает последнюю цифру данного числа
-Console.Write("Введите трехзначное число: ");
+Console.WriteLine("Введите число: ");
 int n = int.Parse(Console.ReadLine());
-int result = n%10;
-Console.Write(result);
+int count = -n;
+while(count<=n)
+{
+   Console.WriteLine(count);
+    count++;
+}
+
+// //Написать программу, которая на вход принимает трехзначное число и на выходе дает последнюю цифру данного числа
+// Console.Write("Введите трехзначное число: ");
+// int n = int.Parse(Console.ReadLine());
+// int result = n%10;
+// Console.Write(result);
 
 
