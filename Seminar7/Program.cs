2978fbe14f688ae92173edb1ef343d2167ae8ba6
Elena Zosina <z-len@yandex.ru>
2023-02-11 17:31:19 +0300

    Семинар 7

--- Seminar7/Program.cs
+++ Seminar7/Program.cs
@@ -158,3 +158,58 @@ m = 3, n = 4.
 Сумма элементов главной
 диагонали: 1+9+2 = 12*/
 
+// int[,] GetArray(int m, int n, int minValue, int maxValue)
+// {
+//     int[,] result = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++) 
+//         {
+//             result[i, j] = new Random().Next(minValue, maxValue + 1);
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[,] inArray)
+// {
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Console.Write($"{inArray[i, j]}\t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int Summ(int[,] arr)
+// {
+        
+//         int summ = 0;
+//         for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+            
+//             if(i==j) summ = summ + arr[i,j];
+//         }
+        
+//     }
+//     return summ;
+// }
+
+// Console.Clear();
+
+// Console.Write("Введите количество строк: ");
+// int row = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите количество столбцов: ");
+// int col = int.Parse(Console.ReadLine()!);
+
+// int[,] array = GetArray(row, col, 0, 20);
+// PrintArray(array);
+// Console.WriteLine();
+
+// int result = Summ(array);
+// Console.WriteLine(result);
+
